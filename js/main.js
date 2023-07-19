7a33b53895d56256792686a38d23e03605c7bba4
Pietro-Leone <leonepietro1999@gmail.com>
2023-07-19 16:00:47 +0200

    aggiunto super bonus

--- js/main.js
+++ js/main.js
@@ -5,10 +5,21 @@ Vue.createApp({
     data() {
         return {
             text: "Hello World!",
-            titleClasses: "text-center display-1 text-info",
+            titleClasses: "text-center display-1 text-info mt-5",
+            image: "img/vue.jpg",
+            imgClasses: "d-block m-auto"
         };
     },
     methods: {
-
+        changeImg() {
+            if (this.text.includes("Mio")) {
+                this.image = "img/vue.jpg";
+                this.text = "Hello World!";
+            }
+            else {
+                this.image = "img/Mio_Padre_Valerio.png";
+                this.text = "Mio Padre Valerio";
+            }
+        },
     },
 }).mount("#app");
