4b72ad0d947fd54afabcf0ccda11e30cb1ec9fc4
Pietro-Leone <leonepietro1999@gmail.com>
2023-07-19 15:36:28 +0200

    creazione file e fine consegna

---
+++ js/main.js
@@ -0,0 +1,14 @@
+"use script"
+
+Vue.createApp({
+
+    data() {
+        return {
+            text: "Hello World!",
+            titleClasses: "text-center display-1 text-info",
+        };
+    },
+    methods: {
+
+    },
+}).mount("#app");
